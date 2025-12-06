4fe581d31409c6d0aaabfd5153b08dea8a70e9b7
Gael Arzubide <arzubide.ga@gmail.com>
2025-12-06 12:21:06 -0600

    se agrega pestaña de favoritosÑ

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,10 +1,37 @@
+"use client";
 import Header from "@/app/components/Header";
+import {usePokemonState} from "@/app/store/store";
+import Link from "next/link";
+import PokemonDetailCard from "@/app/components/PokemonDetailCard";
 
-export default function page() {
-
+export default function FavouritesPage() {
+    const {favorites}  = usePokemonState();
     return (
         <>
-            <Header/>
+            <Header />
+            <div className="p-8 min-h-screen bg-gray-50">
+
+                {favorites.length === 0 ? (
+                    <div className="text-center py-20">
+                        <p className="text-xl text-gray-500 mb-4">No pokemon yet</p>
+                        <Link
+                            href="/pokemon"
+                            className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition"
+                        >
+                            Add Pokemon
+                        </Link>
+                    </div>
+                ) : (
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                        {favorites.map((pokemon) => (
+                            <div key={pokemon.name} className="w-full transform hover:scale-105 transition duration-300">
+                                <PokemonDetailCard pokemon={pokemon} />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </>
-    )
+    );
 }
