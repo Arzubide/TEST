3821495a2f0b979910a6a5b0fb7b5e51bfb2e068
Gael Arzubide <arzubide.ga@gmail.com>
2025-12-06 10:43:24 -0600

    se agrega add to favorites a la carta de detalles

--- app/components/PokemonDetailCard.tsx
+++ app/components/PokemonDetailCard.tsx
@@ -1,49 +1,67 @@
+"use client"; //
 import Link from "next/link";
 import {PokemonDetail} from "@/app/schemas/pokemonSchemas";
+import {useState,useEffect} from "react";
+import {usePokemonState} from "@/app/store/store";
+
 
 interface PokemonDetailCardProps {
     pokemon: PokemonDetail;
 }
 
 export default function PokemonDetailCard({ pokemon }: PokemonDetailCardProps) {
-  return (
-    <>
-        <div className="flex flex-col items-center justify-center min-h-screen p-8">
-            <div className="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full text-center border">
+    const {favorites,handleClickFavorite} = usePokemonState()
 
-                <h1 className="text-3xl font-bold capitalize mb-4">{pokemon.name}</h1>
+    const isFavorite = favorites.some(fav => fav.name === pokemon.name)
 
-                {pokemon.sprites.front_default && (
-                    <img
-                        src={pokemon.sprites.front_default}
-                        alt={pokemon.name}
-                        className="w-48 h-48 mx-auto object-contain"
-                    />
-                )}
+        return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen p-8">
+                <div className="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full text-center border">
 
-                <div className="flex justify-center gap-2 mb-4">
-                    {pokemon.types.map((t) => (
-                        <span
-                            key={t.type.name}
-                            className="px-3 py-1 bg-gray-200 rounded-full text-sm capitalize"
-                        >
-                            {t.type.name}
-                        </span>
-                    ))}
+                    <h1 className="text-3xl font-bold capitalize mb-4">{pokemon.name}</h1>
+
+                    {pokemon.sprites.front_default && (
+                        <img
+                            src={pokemon.sprites.front_default}
+                            alt={pokemon.name}
+                            className="w-48 h-48 mx-auto object-contain"
+                        />
+                    )}
+
+                    <div className="flex justify-center gap-2 mb-4">
+                        {pokemon.types.map((t) => (
+                            <span
+                                key={t.type.name}
+                                className="px-3 py-1 bg-gray-200 rounded-full text-sm capitalize"
+                            >
+                                {t.type.name}
+                            </span>
+                        ))}
+                    </div>
+
+                    <p className="text-gray-600 mb-6">
+                        Weight: <span className="font-bold">{pokemon.weight / 10} kg</span>
+                    </p>
+
+                    <button
+                        onClick={() => handleClickFavorite(pokemon)}
+                        className={`rounded-2xl hover: cursor-pointer ${
+                            isFavorite
+                                ? "bg-gray-300  scale-110" 
+                                : "bg-yellow-300 text-gray-400" 
+                        }`}
+                    >
+                        {isFavorite ? "Remove Favorite" : "Add to Favorite"}
+                    </button>
+                    <Link
+                        href="/pokemon"
+                        className="text-blue-500 hover:underline text-sm"
+                    >
+                        ← Volver al listado
+                    </Link>
                 </div>
-
-                <p className="text-gray-600 mb-6">
-                    Weight: <span className="font-bold">{pokemon.weight / 10} kg</span>
-                </p>
-
-                <Link
-                    href="/pokemon"
-                    className="text-blue-500 hover:underline text-sm"
-                >
-                    ← Volver al listado
-                </Link>
             </div>
-        </div>
-    </>
-  )
+        </>
+      )
 }
